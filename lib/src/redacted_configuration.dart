1df7738ebb970d1c16f921023f57f8e34c286a95
AbdulMunim <amunim1999@gmail.com>
2024-11-23 17:05:48 +0500

    fix: null check issue

--- lib/src/redacted_configuration.dart
+++ lib/src/redacted_configuration.dart
@@ -4,13 +4,13 @@ class RedactedConfiguration {
   bool autoFillTexts;
   String autoFillText;
   Color? redactedColor = Colors.grey.shade400.withAlpha(100);
-  Duration animationDuration;
+  Duration? animationDuration = const Duration(seconds: 5);
 
   RedactedConfiguration({
     this.autoFillText =
         "This is a place holder text to fill the space for redaction",
     this.autoFillTexts = true,
-    this.animationDuration = const Duration(milliseconds: 800),
-      this.redactedColor,
+    this.animationDuration,
+    this.redactedColor,
   });
 }
